17d68ea7e2220b8fbaf2c11d88266955e115c9d6
vboxuser <vboxuser@linuxvm.myguest.virtualbox.org>
2024-03-22 10:32:06 +0530

    src file

---
+++ src/main/java/com/cst/appDemo/Home.java
@@ -0,0 +1,36 @@
+package com.cst.appDemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    Logger logger = LoggerFactory.getLogger(Home.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+    @RequestMapping("home")
+    public String indexError() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+
+}
---
+++ src/test/java/com/cst/appDemo/AppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst.appDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
